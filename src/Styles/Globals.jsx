c13b640adeba59051c63a37609453c1044471a19
Uriel <urieligara@hotmail.com>
2022-10-27 00:37:09 -0300

    styled-components

---
+++ src/Styles/Globals.jsx
@@ -0,0 +1,45 @@
+import styled, {createGlobalStyle} from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+  ::-webkit-scrollbar {
+    width: 13px;
+  }
+  ::-webkit-scrollbar-track {
+    background: #ffffff;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #b10f84;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #a01379;
+  }
+  body {
+    font-family: Open Sans, sans-serif;
+  }
+  a {
+    text-decoration: none;
+    cursor: pointer;
+    color: #e91e63;
+  }
+  ul {
+    list-style: none;
+  }
+  button {
+    text-decoration: none;
+    border: 0;
+    cursor: pointer;
+  }
+`;
+
+export const Container = styled.div`
+	padding: 0rem 3.75rem;
+	@media (max-width: 450px) {
+		padding: 0rem 1rem;
+	}
+`;
