00edc3bb5fea0e6a6b32846646eea80f4fc1c47a
Uriel <urieligara@hotmail.com>
2022-10-28 06:44:10 -0300

    updated conditional

--- src/Components/SearchResults/Cards/index.jsx
+++ src/Components/SearchResults/Cards/index.jsx
@@ -4,7 +4,24 @@ import * as Styled from './Styled';
 export default function Cards({data}) {
 	return (
 		<Styled.Container>
-			{data &&
+			{data.name ? (
+				<Styled.Card key={data.id}>
+					<Styled.Picture>
+						<img src={data.sprites.front_default} alt='' width='90' />
+					</Styled.Picture>
+					<Styled.Divider />
+					<Styled.Title>{data.name}</Styled.Title>
+					<Styled.Options>
+						<Styled.Button mright type='button'>
+							<Styled.Icon src='img/icons/trash.svg' alt='Excluir' /> Excluir
+						</Styled.Button>
+						<Styled.Button type='button'>
+							<Styled.Icon src='img/icons/edit.svg' alt='Editar' /> Editar
+						</Styled.Button>
+					</Styled.Options>
+				</Styled.Card>
+			) : (
+				data &&
 				data?.map((item) => {
 					return (
 						<Styled.Card key={item.data.id}>
@@ -23,7 +40,8 @@ export default function Cards({data}) {
 							</Styled.Options>
 						</Styled.Card>
 					);
-				})}
+				})
+			)}
 		</Styled.Container>
 	);
 }
