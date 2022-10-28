72d219e54155b5d4dd75c09cb150ea720109b94e
Uriel <urieligara@hotmail.com>
2022-10-28 06:43:05 -0300

    created onSubmit

--- src/Components/Header/SearchBar/index.jsx
+++ src/Components/Header/SearchBar/index.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import * as Styled from './Styled';
+import {DataOnSearch} from '../../../Services/data';
+import {Pokemon} from '../../../GlobalStates/contexts';
 
 export default function SearchBar() {
+	const {setPokemon} = useContext(Pokemon);
+	const [value, setValue] = useState('');
+
+	function onSubmit() {
+		DataOnSearch(value, setPokemon);
+	}
 	return (
 		<>
-			<Styled.IconBar />
-			<Styled.SearchBar name='search' placeholder='Digite aqui a sua busca...' />
+			<Styled.IconBar onClick={() => onSubmit()} />
+			<Styled.SearchBar
+				name='search'
+				onChange={(e) => setValue(e.target.value)}
+				placeholder='Digite aqui a sua busca...'
+			/>
 		</>
 	);
 }
