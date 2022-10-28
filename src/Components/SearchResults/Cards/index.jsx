71eca5c3c63a7b3842bdfb4f45a47e17e6fc040b
Uriel <urieligara@hotmail.com>
2022-10-28 04:11:58 -0300

    created cards

---
+++ src/Components/SearchResults/Cards/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import * as Styled from './Styled';
+
+export default function Cards({data}) {
+	return (
+		<Styled.Container>
+			{data &&
+				data?.map((item) => {
+					return (
+						<Styled.Card key={item.data.id}>
+							<Styled.Picture>
+								<img src={item.data.sprites.front_default} alt='' width='90' />
+							</Styled.Picture>
+							<Styled.Divider />
+							<Styled.Title>{item.data.name}</Styled.Title>
+							<Styled.Options>
+								<Styled.Button mright type='button'>
+									<Styled.Icon src='img/icons/trash.svg' alt='Excluir' /> Excluir
+								</Styled.Button>
+								<Styled.Button type='button'>
+									<Styled.Icon src='img/icons/edit.svg' alt='Editar' /> Editar
+								</Styled.Button>
+							</Styled.Options>
+						</Styled.Card>
+					);
+				})}
+		</Styled.Container>
+	);
+}
