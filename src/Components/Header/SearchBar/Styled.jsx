058b872ea8df2e82ac8d082439d9064fce3e7921
Uriel <urieligara@hotmail.com>
2022-10-29 20:55:46 -0300

    updated IconBar

--- src/Components/Header/SearchBar/Styled.jsx
+++ src/Components/Header/SearchBar/Styled.jsx
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 export const SearchBar = styled.input`
 	width: 100%;
-	height: 4rem;
+	height: 5rem;
 	border-radius: 8px;
 	padding: 0 2rem;
-	font-size: 1.3rem;
+	font-size: 1.8rem;
 
 	::-webkit-input-placeholder {
 		opacity: 0.7;
@@ -13,15 +13,15 @@ export const SearchBar = styled.input`
 `;
 
 export const IconBar = styled.span`
-	height: 2rem;
-	width: 2rem;
-	background: url('img/icons/search.png') no-repeat;
+	height: 3rem;
+	width: 3rem;
+	background: url(img/icons/search.png) no-repeat;
 	background-size: cover;
 	padding: 4px;
 	position: absolute;
-	box-sizing:border-box;
-	right: 11.7rem;
-	top: 8.7rem;
+	box-sizing: border-box;
+	right: 12rem;
+	top: 12.6rem;
 	cursor: pointer;
 }
 `;
