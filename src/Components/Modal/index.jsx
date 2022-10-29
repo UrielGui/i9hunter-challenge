600a209b10a265287516b10715f1d8e08d8fe64e
Uriel <urieligara@hotmail.com>
2022-10-29 20:59:51 -0300

    created Modal

---
+++ src/Components/Modal/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewCard from './NewCard';
+import DeleteCard from './DeleteCard';
+
+export default function Modal() {
+	return (
+		<>
+			<NewCard />
+			<DeleteCard />
+		</>
+	);
+}
