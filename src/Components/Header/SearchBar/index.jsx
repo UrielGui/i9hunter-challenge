f004fe87a82923624d0999f6a87e5f1e8c7b9b86
Uriel <urieligara@hotmail.com>
2022-10-29 21:15:36 -0300

    created keyDownHandler

--- src/Components/Header/SearchBar/index.jsx
+++ src/Components/Header/SearchBar/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect, useCallback} from 'react';
 import * as Styled from './Styled';
 import {DataOnSearch} from '../../../Services/data';
 import {Pokemon} from '../../../GlobalStates/contexts';
@@ -7,9 +7,23 @@ export default function SearchBar() {
 	const {setPokemon} = useContext(Pokemon);
 	const [value, setValue] = useState('');
 
-	function onSubmit() {
+	const onSubmit = useCallback(() => {
 		DataOnSearch(value, setPokemon);
-	}
+	}, [setPokemon, value]);
+
+	useEffect(() => {
+		const keyDownHandler = (event) => {
+			if (event.key === 'Enter') {
+				event.preventDefault();
+				onSubmit();
+			}
+		};
+		document.addEventListener('keydown', keyDownHandler);
+		return () => {
+			document.removeEventListener('keydown', keyDownHandler);
+		};
+	}, [onSubmit]);
+
 	return (
 		<>
 			<Styled.IconBar onClick={() => onSubmit()} />
