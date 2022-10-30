fdc394361b0154debaa939661dfa6bfcf5823278
Uriel <urieligara@hotmail.com>
2022-10-29 21:16:05 -0300

    added Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './Components/Header';
 import SearchResults from './Components/SearchResults';
 import States from './GlobalStates/States';
+import Modal from './Components/Modal';
 
 export default function App() {
 	return (
 		<States>
+			<Modal />
 			<Header />
 			<SearchResults />
 		</States>
