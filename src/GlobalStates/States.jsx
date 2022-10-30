1121fbdc6d5484a35379c6bb349d3e3907fb2253
Uriel <urieligara@hotmail.com>
2022-10-29 21:16:55 -0300

    added Modal Provider

--- src/GlobalStates/States.jsx
+++ src/GlobalStates/States.jsx
@@ -1,12 +1,21 @@
 import React, {useState} from 'react';
-import {Pokemon} from './contexts';
+import {Pokemon, Modal} from './contexts';
 
 export default function States({children}) {
 	const [pokemon, setPokemon] = useState('');
 	const [offset, setOffset] = useState(0);
+	const [showRemoveModal, setShowRemoveModal] = useState(false);
+	const [showNewCardModal, setShowNewCardModal] = useState(false);
+	const [editCard, setEditCard] = useState('');
 	return (
 		<>
-			<Pokemon.Provider value={{pokemon, setPokemon, offset, setOffset}}>{children}</Pokemon.Provider>
+			<Pokemon.Provider value={{pokemon, setPokemon, offset, setOffset}}>
+				<Modal.Provider
+					value={{showNewCardModal, setShowNewCardModal, showRemoveModal, setShowRemoveModal, editCard, setEditCard}}
+				>
+					{children}
+				</Modal.Provider>
+			</Pokemon.Provider>
 		</>
 	);
 }
